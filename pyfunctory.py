958fbbfb5c969d13a304604cf7b7a4dd13925f8c
James Thomas <F0llydog@protonmail.com>
2017-07-21 17:44:17 +0100

    utilities added

--- pyfunctory.py
+++ pyfunctory.py
@@ -50,8 +50,36 @@ NUGGETS
 """
 UTILITIES
 
-"""
+Small, functional-style utilites.
 
+"""
+def take(n, iterator):
+    """ Return the first n elements. """
+    return itertools.islice(iterator, n)
+
+
+def drop(n, it):
+    """ Return all but the first n elements """
+    return itertools.islice(it, n, None)
+
+
+def tail(it):
+    """ Return all but the first element. """
+    t = functools.partial(drop, 1)
+    return t(it)
+
+
+def iterate(f, x):
+    """
+    Applies f to x, then f(f(x)), then f(f(f(x))), etc.
+
+    Example: apply double three times to an x of 2
+    >>> double = lambda x: x + x
+    >>> take(4, iterate(double, 2))
+    [2, 4, 8, 16]
+
+    """
+    return itertools.accumulate(itertools.repeat(x), lambda fx, _: f(fx))
 
 
 """"
@@ -89,6 +117,15 @@ generators and larger program flow sequences.
 
 """
 def make_partial(func, *args, **kwargs):
+    """
+    Return partially applied function for use in pipelines.
+
+    Example:
+    >>> add2 = make_partial(operator.add, 2)
+    >>> add2(10)
+    >>> 12
+
+    """
     return functools.partial(func, *args, **kwargs)
 
 
