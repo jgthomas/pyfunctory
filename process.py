746ba8293b51fdc09291f295661905137e5eba95
James Thomas <dairy_water@yahoo.co.uk>
2017-07-22 11:31:01 +0100

    declarations more general

--- process.py
+++ process.py
@@ -17,12 +17,12 @@ def feed_filter(feed, criterion=None, *comparison):
         yield element
 
 
-def load_words(word_file, criterion=None, *comparison):
-    with open(word_file) as filename:
-        return [word for word
+def load_data(data_file, criterion=None, *comparison):
+    with open(data_file) as filename:
+        return [line for line
                 in feed_filter(filename, criterion, *comparison)]
 
 
-def data_filter(data, criterion=None, *comparison):
+def filter_data(data, criterion=None, *comparison):
     return [element for element
             in feed_filter(data, criterion, *comparison)]
