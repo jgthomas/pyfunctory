e069d9786b659ba7471d3ae26bd19da593ff3fde
James Thomas <dairy_water@yahoo.co.uk>
2017-07-23 11:24:24 +0100

    add regex match_factory

--- nuggets.py
+++ nuggets.py
@@ -9,9 +9,9 @@ Regex patterns for filtering purposes.
 import re
 
 
-initial_lowercase = re.compile(r'^[a-z]')
+ini_lower = re.compile(r'^[a-z]')
 
-initial_capital = re.compile(r'^[A-Z]')
+ini_upper = re.compile(r'^[A-Z]')
 
 all_lower = re.compile(r'^[a-z]+$')
 
@@ -22,3 +22,57 @@ all_word_chars = re.compile(r'\w+$')
 all_non_word_chars = re.compile(r'\W+$')
 
 ends_ss = re.compile(r'^[A-Za-z]+ss$')
+
+
+def match_factory(*nuggets, match=True):
+    """ Return function to test item against all specified regex patterns. """
+    def regex_match(item):
+        """
+        Return True if item matches all nuggets.
+
+        i.e. one failed match will result in it returning False.
+
+        Example:
+        >>> match = match_factory(ini_cap, all_word_chars)
+        >>> match("Dog")
+        >>> True
+        >>> match("Dog1")
+        >>> False
+        >>> match("dog")
+        >>> False
+        >>> match("dog1")
+        >>> False
+        """
+        for nugget in nuggets:
+            if nugget.match(item):
+                continue
+            else:
+                return False
+        return True
+    def regex_not_match(item):
+        """
+        Return True if item does NOT match all nuggets.
+
+        i.e. one successful match will result in it returning False
+
+        Example:
+        >>> not_match = match_factory(ini_cap, all_word_chars, match=False)
+        >>> not_match("Dog")
+        >>> False
+        >>> not_match("Dog1")
+        >>> False
+        >>> not_match("dog")
+        >>> False
+        >>> not_match("dog1")
+        >>> True
+
+        """
+        for nugget in nuggets:
+            if not nugget.match(item):
+                continue
+            else:
+                return False
+        return True
+    if match:
+        return regex_match
+    return regex_not_match
