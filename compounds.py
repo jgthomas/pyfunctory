b2cb43c4e25735155ef98dffc5b4ebd693a76d56
James Thomas <codeactor@protonmail.com>
2017-08-07 22:45:48 +0100

    edit docstring

--- compounds.py
+++ compounds.py
@@ -16,9 +16,9 @@ def can_be_made(ref, item, exact_match=False):
         ref (iterable): basis for comparison
         item (iterable): item to be compared with reference
         exact_match (bool): if True, *every* element in each
-                            iterable must have a matching element
-                            in the other iterable, otherwise item
-                            need only match a subset of ref
+                    iterable must have a matching element in the
+                    other iterable, otherwise item need only match
+                    a subset of ref
 
     Returns:
         bool: True if can be made, otherwise False.
